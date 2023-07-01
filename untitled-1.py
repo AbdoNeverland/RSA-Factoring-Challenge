87b1858c0a3692be4ffe2d97641f13b4f9be1890
abdoneverland <ch.abdel.news@gmail.com>
2023-07-01 22:00:34 +0100

    task 0

--- untitled-1.py
+++ untitled-1.py
@@ -1,34 +1,65 @@
-import math
-c = 1
-def gcd(x,y):
-    if y == 0:
-        return x
-    return gcd(y, x % y)
-def g(x, c):
-    return (x*x +c)
+import random
+from random import randrange
 
-def getf(n):
-    global c
-    if (n == 1):
-        return n    
-    x = y = 2
-    c +=1
-    d=1
+def is_large_prime(n, k=5): # miller-rabin
+    from random import randint
+    if n < 2: return False
+    for p in [2,3,5,7,11,13,17,19,23,29]:
+        if n % p == 0: return n == p
+    s, d = 0, n-1
+    while d % 2 == 0:
+        s, d = s+1, d>>1
+    for i in range(k):
+        x = pow(randint(2, n-1), (d), n)
+        if x == 1 or x == n-1: continue
+        for r in range(1, s):
+            x = (x * x) % n
+            if x == 1: return False
+            if x == n-1: break
+        else: return False
+    return True
+# Iterative Function to calculate
+# (a^n)%p in O(logy)
+def power(a, n, p):
 
-    while d ==1:
-        x=g(x,c) % n
-        y=g(g(y, c),c) %n
-        y=g(g(y, c),c) %n
-        d=gcd(abs(x - y), n)
+    # Initialize result
+    res = 1
 
-    if d == n: 
-        return getf(n)
-    else:
-        return d
-def isPrime(n):
-    for i in range(2, int(math.sqrt(n))):
+    # Update 'a' if 'a' >= p
+    a = a % p 
+
+    while n > 0:
+
+        # If n is odd, multiply
+        # 'a' with result
+        if n % 2:
+            res = (res * a) % p
+            n = n - 1
+        else:
+            a = (a ** 2) % p
+
+            # n must be even now
+            n = n // 2
+
+    return res % p
+
+# If n is prime, then always returns true,
+# If n is composite than returns false with
+# high probability Higher value of k increases
+# probability of correct result
+def is_prime_smaller(n):
+    for i in range(2, n // 2):
         if n%i == 0:
-            return i
-    return -1
-print(isPrime(691226912809))
+            return False
+    return True
+def is_prime(n, prec=3):
+    if n < 100:
+        return is_prime_smaller(n)
+    else:
+        a = randrange(2, n - 2+1)
+        if pow(a, n - 1, n) != 1:
+            return False
+
+    return True
+print(is_prime(37))
 #print(getf(193993833211))
